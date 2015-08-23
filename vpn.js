d9ea37498d3a1d8b88e75ce93688217f0bd86c9a
Jack Peterson <jack@tinybike.net>
2015-08-23 16:52:27 -0700

    Organized code a bit, added module.exports

--- vpn.js
+++ vpn.js
@@ -12,66 +12,74 @@ var log = console.log;
 /* uncomment the next line to use sheldon.forestry.oregonstate.edu */
 // config.db.server = "sheldon.forestry.oregonstate.edu";
 
-function doStuff() {
+var vpn = {
 
-    /* insert commands you want to carry out while connected to the vpn here */
+    doStuff: function () {
+        var self = this;
+    
+        /* insert commands you want to carry out while connected to the vpn here */
 
-    var conn = new db.Connection(config.db, function (err) {
-        if (err) throw err;
-
-        var query = "SELECT TOP 1 * FROM metdat.dbo.phrsc_table2 ORDER BY tmstamp ASC";
-        var request = conn.request();
-        request.query(query, function (err, recordset) {
+        var conn = new db.Connection(config.db, function (err) {
             if (err) throw err;
-            console.dir(recordset);
-            disconnect();
-        });    
-    });
 
-    conn.on("error", function (err) {
-        if (err) log("Database error:", err);
-    });
-}
-
-function disconnect() {
-    vpnc.disconnect(function (err, code) {
-        if (err) {
-            log("Error disconnecting VPN:", err);
-        } else {
-            log("VPN", chalk.yellow("disconnected [" + code + "]"));
-            process.exit(0);
-        }
-    });
-}
-
-function connect() {
-    vpnc.connect(config.vpn, function (err, code) {
-        if (err) {
-            log("Error connecting VPN:", err);
-        } else {
-
-            // did we connect to the VPN successfully?
-            // (check for tun0, tun1, etc. using ifconfig)
-            cp.exec("ifconfig tun", function (err, stdout) {
-                if (err) {
-                    log(chalk.red.bold("Unable to connect to VPN"));
-                    disconnect();
-                }
-                log("VPN", chalk.cyan("connected [" + code + "]"));
-                log(chalk.green(stdout));
-                doStuff();
+            var query = "SELECT TOP 1 * FROM metdat.dbo.phrsc_table2 ORDER BY tmstamp ASC";
+            var request = conn.request();
+            request.query(query, function (err, recordset) {
+                if (err) throw err;
+                console.dir(recordset);
+                self.disconnect();
             });
-        }
-    });
-}
+        });
+
+        conn.on("error", function (err) {
+            if (err) log("Database error:", err);
+        });
+    },
+
+    disconnect: function () {
+        vpnc.disconnect(function (err, code) {
+            if (err) {
+                log("Error disconnecting VPN:", err);
+            } else {
+                log("VPN", chalk.yellow("disconnected [" + code + "]"));
+                // uncomment next line to quit Node automatically on disconnect
+                // process.exit(0);
+            }
+        });
+    },
+
+    connect: function () {
+        var self = this;
+        vpnc.connect(config.vpn, function (err, code) {
+            if (err) {
+                log("Error connecting VPN:", err);
+            } else {
+
+                // did we connect to the VPN successfully?
+                // (check for tun0, tun1, etc. using ifconfig)
+                cp.exec("ifconfig tun", function (err, stdout) {
+                    if (err) {
+                        log(chalk.red.bold("Unable to connect to VPN"));
+                        self.disconnect();
+                    }
+                    log("VPN", chalk.cyan("connected [" + code + "]"));
+                    log(chalk.green(stdout));
+                    self.doStuff();
+                });
+            }
+        });
+    }
+};
 
 vpnc.available(function (err, version) {
     if (err) {
         log("vpnc unavailable:", err);
     } else {
         log("Found vpnc:", chalk.gray(JSON.stringify(version, null, 2)));
-        connect();
+        vpn.connect();
     }
 });
 
-process.on("exit", function (code) { if (code) disconnect(); });
+process.on("exit", function (code) { if (code) vpn.disconnect(); });
+
+module.exports = vpn;
